40e889bd1ae6874d4268e4ba671a73026d48a332
Amr Shady <amrsabdelhalem@gmail.com>
2024-10-21 19:00:02 +0300

    data cleaning script created

--- glassdoor.py
+++ glassdoor.py
@@ -4,5 +4,5 @@ import pandas as pd
 path = "C:/Users/Lenovo/Documents/ds_salary_project/chromedriver.exe"
 df= gs.get_jobs('data scientist', 1000, False, path, 15)
 
-df.to_csv('glasdoor_scraping.csv', index=False)
+df.to_csv('glassdoor_scraping.csv', index=False)
 print(df)
