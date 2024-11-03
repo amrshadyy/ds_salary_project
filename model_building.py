48ae843d4e891d3a9b09c3cd49f161d2d7d9c326
Amr Shady <amrsabdelhalem@gmail.com>
2024-11-03 12:36:56 +0200

    updated model_building

--- model_building.py
+++ model_building.py
@@ -44,12 +44,13 @@ from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import cross_val_score
 
 lm = LinearRegression()
-lm.fit(X_train, y_train)
+lm.fit(X_train,y_train)
 
 np.mean(cross_val_score(lm, X_train, y_train, scoring = 'neg_mean_absolute_error'))
 
 # lasso regression
-lm_l = Lasso()
+lm_l = Lasso(alpha=0.01)
+lm_l.fit(X_train,y_train)
 np.mean(cross_val_score(lm_l, X_train, y_train, scoring = 'neg_mean_absolute_error'))
 
 alpha = []
@@ -80,4 +81,16 @@ parameters = {'n_estimators':range(10,300,10), 'criterion':('squared_error','abs
 gs = GridSearchCV(rf,parameters,scoring= 'neg_mean_absolute_error' )
 gs.fit(X_train,y_train)
 gs.best_score_
+gs.best_estimator_.get_params()
+
 # test ensembles
+tpred_lm = lm.predict(X_test)
+tpred_lml = lm_l.predict(X_test)
+tpred_rf = gs.best_estimator_.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_test,tpred_lm)
+mean_absolute_error(y_test,tpred_lml)
+mean_absolute_error(y_test,tpred_rf)
+
+mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2)
